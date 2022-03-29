a182dbd998c60fde932c245ef3608ad1b63c0d05
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2022-03-30 02:09:59 +0700

    Add scikit-learn

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -e
 
 # install dependencies
-# apk add --no-cache gcc musl-dev
+apk add --no-cache gcc musl-dev
+apk add gfortran build-base wget libpng-dev openblas-dev
 
 # build
 pip wheel --no-deps --requirement requirements.txt
