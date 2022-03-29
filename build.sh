52e8a0e8ef9f97a8572a32a4f5bb78203ba3f4a7
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2022-03-30 02:17:47 +0700

    Add --extra-index-url

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ apk add --no-cache gcc musl-dev
 apk add gfortran build-base wget libpng-dev openblas-dev
 
 # build
-pip wheel --no-deps --requirement requirements.txt
+pip wheel --extra-index-url https://alpine-wheels.github.io/index --no-deps --requirement requirements.txt
