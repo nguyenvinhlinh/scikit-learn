76641f33e2f5b2006c3e3f6ef9f7aee417345e7b
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2022-03-30 02:20:01 +0700

    add --no-cache for apk command

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 # install dependencies
 apk add --no-cache gcc musl-dev
-apk add gfortran build-base wget libpng-dev openblas-dev
+apk add --no-cache gfortran build-base wget libpng-dev openblas-dev
 
 # build
 pip wheel --extra-index-url https://alpine-wheels.github.io/index --no-deps --requirement requirements.txt
